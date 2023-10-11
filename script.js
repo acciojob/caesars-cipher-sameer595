22bced2a4d34b15775859f9ec6d144255901a59a
sameer595 <142083277+sameer595@users.noreply.github.com>
2023-10-11 13:06:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,56 +1,52 @@
 // Your Script here.
 
 const lookup = {
-  A: "N",
-  B: "O",
-  C: "P",
-  D: "Q",
-  E: "R",
-  F: "S",
-  G: "T",
-  H: "U",
-  I: "V",
-  J: "W",
-  K: "X",
-  L: "Y",
-  M: "Z",
-  N: "A",
-  O: "B",
-  P: "C",
-  Q: "D",
-  R: "E",
-  S: "F",
-  T: "G",
-  U: "H",
-  V: "I",
-  W: "J",
-  X: "K",
-  Y: "L",
-  Z: "M",
-  "?": "?",
-  ",": ",",
-};
-
-function rot13(encodedStr) {
- 
-  // Only change code below this line
-for(let i=0; i<encodedStr.length;i++) {
-	encodedStr=encodedStr.toUpperCase();
- let decodedArr = ""; // Your Result goes here
-	if( lookup[encodedStr[i]]){
-	decodedArr = decodedArr + lookup[encodedStr[i]];
-	}
-	   else{
-		   decodedArr +=encodedStr[i];
-	   }
-
-
-  return decodedArr; //return decodedArr
-}
-}
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
-
-// Do not change this line
-window.rot13 = rot13;
+    A: "N",
+    B: "O",
+    C: "P",
+    D: "Q",
+    E: "R",
+    F: "S",
+    G: "T",
+    H: "U",
+    I: "V",
+    J: "W",
+    K: "X",
+    L: "Y",
+    M: "Z",
+    N: "A",
+    O: "B",
+    P: "C",
+    Q: "D",
+    R: "E",
+    S: "F",
+    T: "G",
+    U: "H",
+    V: "I",
+    W: "J",
+    X: "K",
+    Y: "L",
+    Z: "M",
+    "?": "?",
+    ",": ",",
+  };
+  
+  function rot13(encodedStr) {
+    encodedStr = encodedStr.toUpperCase();
+    let decodedArr = "";
+  
+    for (let i = 0; i < encodedStr.length; i++) {
+      const char = encodedStr[i];
+      if (lookup.hasOwnProperty(char)) {
+        decodedArr += lookup[char];
+      } else {
+        decodedArr += char;
+      }
+    }
+  
+    return decodedArr;
+  }
+  
+  // Testing the rot13 function
+  console.log(rot13("SERR YBIR? NPPVBWBO"));
+  window.rot13 = rot13;
